02acf544217b75f6ae49a565a5693201435c0755
torik42 <77811180+torik42@users.noreply.github.com>
2021-11-29 23:32:56 +0100

    redo cli with package commander, add trace generation to cli

--- src/cli.js
+++ src/cli.js
@@ -1,33 +1,76 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra')
-// const path = require('path')
-const yaml = require('js-yaml')
 const kicad_to_ergogen = require('./kicad-to-ergogen')
 const pkg = require('../package.json')
-const yargs = require('yargs')
+const { program }= require('commander')
 
-// command line arguments
-const args = yargs.argv
 
-const config_file = args._[0]
-if (!config_file) {
-    console.error('Usage: kicad-to-ergogen <config_file> [options]')
-    process.exit(1)
+
+const footprints = function(config_file){
+  console.log('Reading config file...')
+
+  // read config file
+  let config_text
+  try {
+      config_text = fs.readFileSync(config_file).toString()
+  } catch (err) {
+      console.error(`Could not read config file "${config_file}":\n${err}`)
+      process.exit(2)
+  }
+
+  // create footprints
+  kicad_to_ergogen.kicad_to_ergogen(config_text, s => console.log(s))
+
+  console.log('Done.')
 }
 
-console.log('Reading config file...')
 
-// read config file
-let config_text
-try {
-    config_text = fs.readFileSync(config_file).toString()
-} catch (err) {
-    console.error(`Could not read config file "${config_file}":\n${err}`)
-    process.exit(2)
+
+const traces = function(unmodified_file, modified_file, out_file) {
+  console.log('Reading files...')
+
+  // read unmodified file
+  let unmodified_text
+  try {
+      unmodified_text = fs.readFileSync(unmodified_file).toString()
+  } catch (err) {
+      console.error(`Could not read unmodified file "${unmodified_file}":\n${err}`)
+      process.exit(2)
+  }
+
+  // read modified file
+  let modified_text
+  try {
+      modified_text = fs.readFileSync(modified_file).toString()
+  } catch (err) {
+      console.error(`Could not read modified file "${modified_file}":\n${err}`)
+      process.exit(2)
+  }
+
+  // create footprints
+  out = kicad_to_ergogen.create_traces_list(unmodified_text, modified_text, s => console.log(s))
+  fs.writeFileSync(out_file, out)
+
+  console.log('Done.')
 }
 
-// create footprints
-kicad_to_ergogen.kicad_to_ergogen(config_text, s => console.log(s))
 
-console.log('Done.')
+
+program
+  .version(pkg.version)
+  
+program
+  .command('footprints <config-file>')
+  .description('create ergogen footprint files from KiCad PCB file')
+  .action(config_file => footprints(config_file))
+
+program
+  .command('traces')
+  .description('create ergogen footprint file with all objects that are in KiCad file <modified> but not <plain>')
+  .argument('<plain>', 'unmodified ergogen PCB output')
+  .argument('<modified>', 'partly routet KiCad File')
+  .requiredOption('-o, --output <file>', 'file for ergogen footprint file containing traces')
+  .action((unmodified_file, modified_file,options) => traces(unmodified_file, modified_file, options.output))
+
+program.parse()
